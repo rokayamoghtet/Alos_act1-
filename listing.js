d809028ba177eabbbf80bc2cefc1ea81a32406d1
rokayamoghtet <101093188+rokayamoghtet@users.noreply.github.com>
2022-04-09 22:49:12 +0100

    Update listing.js

--- listing.js
+++ listing.js
@@ -1,12 +1,12 @@
 var unirest = require("unirest")
 
-var req = unirest("GET", "http://localhost:3000/Livraison")
+var request = unirest("GET", "http://localhost:3000/Livraison")
 
-req.headers({
+request.headers({
     "cache-control": "no-cache", //Indique de renvoyer systématiquement la requête au serveur et ne servir une éventuelle version en cache que dans le cas où le serveur le demande
 })
 
-req.end(function(res){
-    if (res.error) throw new Error(res.error)
+request.end(function(response){
+    if (response.error) throw new Error(response.error)
     console.log(res.body)
 })
